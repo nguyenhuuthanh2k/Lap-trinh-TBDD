d9ad72f506ab67ee34ad33ac9e3af7a252f58023
Thanh <ongvuabom13@gmail.com>
2022-04-11 13:04:41 +0700

    up lan 4

--- BTLayDiemCong/BaiKT1/app/src/main/java/NguyenHuuThanh/BaiKT1/MainActivity.java
+++ BTLayDiemCong/BaiKT1/app/src/main/java/NguyenHuuThanh/BaiKT1/MainActivity.java
@@ -1,34 +1,34 @@
 package NguyenHuuThanh.BaiKT1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText edtUserName,edtPass;
-    Button btnLogin;
+    EditText edtUserName, edtPass;
+    AppCompatButton btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setEvent();
         setControl();
+        setEvent();
     }
 
     private void setEvent() {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //kiem tra
-                if(edtUserName.getText().toString().equals("maicuongtho") && edtPass.getText().toString().equals("Cntt60ntu!")){
-                    Toast.makeText(MainActivity.this,"User and Password is correct",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this,"User and Password is wrong",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                String userName = edtUserName.getText().toString();
+                String passWord = edtPass.getText().toString();
+                if(userName.equals("maicuongtho") && passWord.equals("Cntt60ntu!")){
+                    Toast.makeText(MainActivity.this, "User and Password is correct", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "User and Password is wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
